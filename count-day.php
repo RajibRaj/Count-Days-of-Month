dac419258de8f78da39e7d3d1c3be2b1402d79de
Rajib Dey <raj.kuet@gmail.com>
2020-06-07 15:30:08 +0600

    Update count-day.php

--- count-day.php
+++ count-day.php
@@ -2,4 +2,5 @@
 //returns days in the given month
 $year = 2020;
 $month = 4;
+// print the total days
 echo $days = ($month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year %400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31));
